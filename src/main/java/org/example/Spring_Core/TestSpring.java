ea703cd49125a8397dcc5ac714e0f6a65f74f7d4
dmitrkarpushoff2016 <dmitrkarpushoff2016@yandex.ru>
2022-09-04 17:16:38 +0300

    Version 1.0. Добавил комментарии к классу TestBean.

--- src/main/java/org/example/Spring_Core/TestSpring.java
+++ src/main/java/org/example/Spring_Core/TestSpring.java
@@ -7,14 +7,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class TestSpring {
     public static void main(String[] args) {
+        //ClassPathXmlApplicationContext - обращается к applicationContext.xml,считывает и помещает бины в applicationContext.
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-
+        //Второй аргумент, бин которого хотим получить.
         TestBean testBean = context.getBean("testBean",TestBean.class);
 
         System.out.println(testBean.getName());
-
+        //Закрываем context. Обязательно.
         context.close();
     }
 }
